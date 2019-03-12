31b208776607084de1cbc6737f62927ee8c77fc6
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-03-12 14:47:12 -0400

    more setup/ end of class

--- Quick.java
+++ Quick.java
@@ -1,12 +1,34 @@
 
 
 
-/*return the value that is the kth smallest value of the array. k=0 is the smallest
- */
- public static int quickselect(int[] data, int k)
+//return the value that is the kth smallest value of the array. k=0 is the smallest 
+ public static int quickselect(int[] data, int k){
+ 
+  
+ }
 
-/*Modify the array to be in increasing order.
- */  
- public static void quicksort(int[] data)
+//Modify the array to be in increasing order.
+ public static void quicksort(int[] data){
+ 
+  
+ }
+
+
+/*Modify the array such that:
+ *1. Only the indices from start to end inclusive are considered in range
+ *2. A random index from start to end inclusive is chosen, the corresponding 
+ *   element is designated the pivot element. 
+ *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+ *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+ *@return the index of the final position of the pivot element.
+ */
+
+private static int partition ( int [] data, int start, int end){
+
+
+}
+
+
+
 
 
