c08edd230274973dc591eb343436d2b090d11101
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-03-12 14:39:01 -0400

    given methods

---
+++ Quick.java
@@ -0,0 +1,12 @@
+
+
+
+/*return the value that is the kth smallest value of the array. k=0 is the smallest
+ */
+ public static int quickselect(int[] data, int k)
+
+/*Modify the array to be in increasing order.
+ */  
+ public static void quicksort(int[] data)
+
+
