6bdd0743520fde760ddda99d4b7e415d5a08769a
isulley77 <isulley@stuy.edu>
2019-03-13 14:44:49 -0400

    addded psuedocode

--- Quick.java
+++ Quick.java
@@ -1,23 +1,32 @@
 
 
 
-//return the value that is the kth smallest value of the array. k=0 is the smallest 
+//return the value that is the kth smallest value of the array. k=0 is the smallest
  public static int quickselect(int[] data, int k){
- 
-  
+
+
  }
 
 //Modify the array to be in increasing order.
  public static void quicksort(int[] data){
- 
-  
+
+/*
+  if(lo >= hi){
+    return;
+}
+
+  pivot = partition(data, lo, hi);
+  quicksort(data, , );
+  quickSort(data, pivot, );
+
+
  }
 
 
 /*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
- *2. A random index from start to end inclusive is chosen, the corresponding 
- *   element is designated the pivot element. 
+ *2. A random index from start to end inclusive is chosen, the corresponding
+ *   element is designated the pivot element.
  *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
  *@return the index of the final position of the pivot element.
@@ -28,7 +37,7 @@ private static int partition ( int [] data, int start, int end){
 
 }
 
-
-
-
-
+private int partitionDutch(int[],int lo, int hi){
+    //your code.
+    return j;
+}
