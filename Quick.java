837f3e6d88cb1d201e49aec48798c4f42961f43a
isulley77 <isulley@stuy.edu>
2019-03-26 14:53:50 -0400

    end of class '

--- Quick.java
+++ Quick.java
@@ -1,54 +1,54 @@
+public class Quick{
+
+  //return the value that is the kth smallest value of the array. k=0 is the smallest
+   public static int quickselect(int[] data, int k){
+
+
+   }
+
+  //Modify the array to be in increasing order.
+   public static void quicksort(int[] data){
+
+  /*
+    if(lo >= hi){
+      return;
+  }
+
+    pivot = partition(data, lo, hi);
+    quicksort(data, , );
+    quickSort(data, pivot, );
+
+
+   }
+
+
+  /*Modify the array such that:
+   *1. Only the indices from start to end inclusive are considered in range
+   *2. A random index from start to end inclusive is chosen, the corresponding
+   *   element is designated the pivot element.
+   *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+   *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+   *@return the index of the final position of the pivot element.
+   */
+
+  private static int partition ( int [] data, int start, int end){
+
+   int pivot = data[end];
+
+   for(int i = start; i < end; i++){
+      if(data[i] <= pivot){
+      }
+   }
 
 
 
-//return the value that is the kth smallest value of the array. k=0 is the smallest
- public static int quickselect(int[] data, int k){
 
 
- }
 
-//Modify the array to be in increasing order.
- public static void quicksort(int[] data){
+  }
 
-/*
-  if(lo >= hi){
-    return;
-}
-
-  pivot = partition(data, lo, hi);
-  quicksort(data, , );
-  quickSort(data, pivot, );
-
-
- }
-
-
-/*Modify the array such that:
- *1. Only the indices from start to end inclusive are considered in range
- *2. A random index from start to end inclusive is chosen, the corresponding
- *   element is designated the pivot element.
- *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
- *4. all elements in range that are larger than the pivot element are placed after the pivot element.
- *@return the index of the final position of the pivot element.
- */
-
-private static int partition ( int [] data, int start, int end){
- 
- int pivot = data[end];
- 
- for(int i = start; i < end; i++){
-    if(data[i] <= pivot){
-    }
- }
-     
- 
- 
- 
-
-
-}
-
-private int partitionDutch(int[],int lo, int hi){
-    //your code.
-    return j;
+  private int partitionDutch(int[],int lo, int hi){
+      //your code.
+      return j;
+  }
 }
